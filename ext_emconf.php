2741eb6277fa8251837f504243f29d8cda312739
boresexpress <boresexpress@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-22 14:58:00 +0000

    Manual updated. Version changed.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rlmp_language_detection/trunk@59536 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "rlmp_language_detection".
 #
-# Auto generated 20-04-2010 15:55
+# Auto generated 22-03-2012 18:41
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This plugin detects the visitor\'s preferred language and sets the local configuration for TYPO3\'s language engine accordingly. Both, one-tree and multiple tree concepts, are supported. It can also select from a list of similar languages if the user\'s preferred language does not exist.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '2.3.0',
+	'version' => '2.3.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 1,
+	'clearcacheonload' => 0,
 	'lockType' => '',
 	'author' => 'Joachim Mathes',
 	'author_email' => 't3extensions@punkt.de',
@@ -34,16 +34,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.0.0-0.0.0',
 			'php' => '3.0.0-0.0.0',
-			'cms' => '',
+			'typo3' => '4.0.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:12:"ext_icon.gif";s:4:"184c";s:17:"ext_localconf.php";s:4:"bddb";s:24:"ext_typoscript_setup.txt";s:4:"e372";s:14:"doc/manual.sxw";s:4:"6b50";s:42:"pi1/class.tx_rlmplanguagedetection_pi1.php";s:4:"ff55";}',
+	'_md5_values_when_last_written' => 'a:5:{s:12:"ext_icon.gif";s:4:"184c";s:17:"ext_localconf.php";s:4:"bddb";s:24:"ext_typoscript_setup.txt";s:4:"935c";s:14:"doc/manual.sxw";s:4:"1a0f";s:42:"pi1/class.tx_rlmplanguagedetection_pi1.php";s:4:"62e0";}',
+	'suggests' => array(
+	),
 );
 
 ?>
